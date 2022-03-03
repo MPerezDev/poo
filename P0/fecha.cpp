cde50490ef2b9f12de8e25c46423635b8cf6317d
[manupruiz] <[rayerito23@gmail.com]>
2022-03-03 20:31:39 +0100

    operadores bool (faltan operadores aritmeticos y cadena)

--- P0/fecha.cpp
+++ P0/fecha.cpp
@@ -3,13 +3,13 @@
 #include <ctime>
 #include "fecha.hpp"
 
-Fecha::Fecha(int d,int m, int a): dia{d}, mes{m}, anno{a}{ //Asignamos los valores
+Fecha::Fecha(int d,int m, int a): dia_{d}, mes_{m}, anno_{a}{ //Asignamos los valores
 
     fechaSistema(); //Comprobamos si alguno de los atributos debe ser sustituido por los de la fecha actual del sistema
 
 }
 
-Fecha::Fecha(Fecha& f): dia{f.dia}, mes{f.mes}, anno{f.anno}{} //Constructor de copia (100% mio)
+Fecha::Fecha(Fecha& f): dia_{f.dia_}, mes_{f.mes_}, anno_{f.anno_}{} //Constructor de copia (100% mio)
 
 
 void Fecha::fechaSistema(){
@@ -17,27 +17,89 @@ void Fecha::fechaSistema(){
     std::time_t fechaSFormato = std::time(nullptr); //cogemos la fecha actual del sistema
     std::tm* fechaCFormato = std::localtime(&fechaSFormato); //Transformamos la fecha obtenida, guardándola
                                                         //en el struct std::tm, que tiene unas cuantas variables que vamos a usar ahora
-    if(dia==0) dia = fechaCFormato->tm_mday; //tm_mday tiene el dia del mes en el que estamos
-    if(mes==0) mes = fechaCFormato->tm_mon + 1; //tm_mon cuenta los meses del 0 al 11, por lo que le sumamos 1
-    if(anno==0) anno = fechaCFormato->tm_year + 1900; //tm_year cuenta los años desde 1900, por eso le sumamos esta cantidad
+    if(dia_==0) dia_ = fechaCFormato->tm_mday; //tm_mday tiene el dia del mes en el que estamos
+    if(mes_==0) mes_ = fechaCFormato->tm_mon + 1; //tm_mon cuenta los meses del 0 al 11, por lo que le sumamos 1
+    if(anno_==0) anno_ = fechaCFormato->tm_year + 1900; //tm_year cuenta los años desde 1900, por eso le sumamos esta cantidad
 
 }
 
 Fecha Fecha::operator =(const Fecha& f){
 
-    dia = f.dia;
-    mes = f.mes;
-    anno = f.anno;
+    dia_ = f.dia_;
+    mes_ = f.mes_;
+    anno_ = f.anno_;
 
 }
 
 void Fecha::comprobarFecha(){
 
-    static const int dias [] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+    static const int dias[] = {0,31,28,31,30,31,30,31,31,30,31,30,31}; //Usamos un vector en el que guardamos los días que tiene cada mes (en años no bisiestos)
+    int bisiesto = static_cast<int>(anno_ % 4 == 0 && (anno_ % 400 == 0 || anno_ % 100 != 0));
+    //Usamos static_cast para convertir el resultado de la comprobación para saber si el año en cuestión es bisiesto
+    //El resultado de la comprobación es un bool y lo cambiamos a un int
+
+    if(mes_ < 1 || mes_ > 12){  //Comprobamos que el mes esté entre 1 y 12, si no es así lanzamos una excepción
+        throw Invalida ("Mes incorrecto");
+    }
+
+    if(mes_ == 2){ //Comprobamos que el día esté entre los valores permitidos según el mes (cubrimos el caso de que se trate de un año bisiesto)
+        if(dia_ < 1 || dia_ > dias[mes_] + bisiesto){
+            throw Invalida ("Dia incorrecto");
+        }
+    }else if(dia_ < 1 || dia_ > dias[mes_]){
+        throw Invalida ("Dia incorrecto");
+    }
     
-    
+    if(anno_ < AnnoMin || anno_ > AnnoMax){ //Comprobamos que el año introducido está dentro los valores permitidos
+        throw Invalida ("Anno incorrecto");
+    }
 
 }
+
+bool operator ==(const Fecha& fecha1, const Fecha& fecha2){
+
+    return (fecha1.anno() == fecha2.anno()) && (fecha1.mes() == fecha2.mes()) && (fecha1.dia() == fecha2.dia());
+
+}
+
+bool operator !=(const Fecha& fecha1, const Fecha& fecha2){
+
+    return !(fecha1 == fecha2);
+
+}
+
+bool operator <(const Fecha& fecha1, const Fecha& fecha2){
+
+    if (fecha1.anno() < fecha2.anno()){
+        return true;
+    }else if ( (fecha1.anno() == fecha2.anno()) && (fecha1.mes() < fecha2.mes()) ){ 
+        return true;
+    }else if ( (fecha1.anno() == fecha2.anno()) && (fecha1.mes() == fecha2.mes()) && (fecha1.dia() < fecha2.dia()) ){
+        return true;
+    }else return false;
+
+}
+
+bool operator >(const Fecha& fecha1, const Fecha& fecha2){
+
+    return (fecha2 < fecha1);
+
+}
+
+bool operator <=(const Fecha& fecha1, const Fecha& fecha2){
+
+    return ((fecha1 < fecha2) || (fecha1 == fecha2));
+
+}
+
+bool operator >=(const Fecha& fecha1, const Fecha& fecha2){
+
+    return ((fecha2 < fecha1) || (fecha1 == fecha2));
+
+}
+
+
+
 
 
 
--- P0/fecha.hpp
+++ P0/fecha.hpp
@@ -10,14 +10,37 @@ class Fecha {
 
         static const int AnnoMax = 2037; //Año limite
         static const int AnnoMin = 1902;
-        
+        class Invalida{ //Creamos una clase para las excepciones.
+            public:
+                Invalida(const char* m): motivo{m} {}
+                const char* por_que() const noexcept {return motivo;} //¿Por qué se utiliza el noexcept?
+            private:
+                const char* motivo; 
+
+        };
+        int dia() const noexcept {return dia_;}
+        int mes() const noexcept {return mes_;}
+        int anno() const noexcept {return anno_;}
+
+
     private:
-        int dia;
-        int mes;
-        int anno;
+        int dia_;
+        int mes_;
+        int anno_;
         void fechaSistema(); //fechaSistema sirve para darle los valores de la fecha del sistema
                              // a los atributos de la instancia
         void comprobarFecha();
 };
+
+//A continuación introducimos las sobrecargas de operadores
+//Las hacemos fuera de la clase, por lo que en ellas recurrimos a
+//los observadores
+bool operator ==(const Fecha& fecha1, const Fecha& fecha2);
+bool operator !=(const Fecha& fecha1, const Fecha& fecha2);
+bool operator <(const Fecha& fecha1, const Fecha& fecha2);
+bool operator >(const Fecha& fecha1, const Fecha& fecha2);
+bool operator <=(const Fecha& fecha1, const Fecha& fecha2);
+bool operator >=(const Fecha& fecha1, const Fecha& fecha2);
+
 
 #endif
